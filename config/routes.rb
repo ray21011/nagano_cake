18395a854a8e7e18b694503bda878cc5bbfadd24
ray21011 <137093132+ray21011@users.noreply.github.com>
2023-08-28 17:08:26 +0000

    [Finish]ユーザーログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  root to: "public/homes#top"
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+get '/admin' => 'admin/homes#top'
+get '/admins/sgin_in' => 'admin/session#new'
+  devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+get '/customers/sign_in' => 'public/session#new'
+get '/customers/mypage' => 'customers#mypage'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
